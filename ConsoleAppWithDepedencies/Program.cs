351d74e2132402719e3c3574ba0bfe84bb9ce03a
Thet Naing Oo <thetnaing94@gmail.com>
2025-09-27 09:31:35 +0630

    1 st Commit

--- ConsoleAppWithDepedencies/Program.cs
+++ ConsoleAppWithDepedencies/Program.cs
@@ -10,6 +10,37 @@ if (blogs.Count == 0)
     return;
 }
 
+Console.Write("Menu : L - List All Blogs, S - Search Blog By Id, E - Exit the Program : ");
+
+string input = Console.ReadLine().ToUpper();
+
+while (input != "L" && input != "S" && input != "E")
+{
+    Console.Write("Invalid Input. Please Enter L or S or E : ");
+    input = Console.ReadLine().ToUpper();
+}
+
+// Exit the Program
+if (input == "E")
+{  
+    return;
+}
+
+#region ListBlog
+else if (input == "L")
+{
+    foreach (var item in blogs)
+    {
+        Console.WriteLine($@"Id : {item.BlogId} 
+        - Title : {item.BlogTitle} 
+        - Author : {item.BlogAuthor} 
+        - Content : {item.BlogContent}");
+    }
+    return;
+}
+#endregion
+
+#region SearchById
 Console.Write("Enter Id of the Blog : ");
 
 int id = int.TryParse(Console.ReadLine(), out var tempId) ? tempId : 0;
@@ -34,3 +65,4 @@ else
     Console.WriteLine("Blog Not Found");
     return;
 }
+#endregion
